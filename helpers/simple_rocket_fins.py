11872ed3f8efea8e63e17875018e1826c29dd2d7
Sidney Pauly <sidney.pauly@venios.de>
2023-10-02 14:33:59 +0100

    Updated measurement data access example Added requirements Cleaned up performance calculations

--- helpers/simple_rocket_fins.py
+++ helpers/simple_rocket_fins.py
@@ -1,5 +1,5 @@
 
-from math import pi, sin, pow
+from math import pi, sin, cos, pow, atan2
 import numpy as np
 
 def get_simple_rocket_FINS_change_equation_and_conditions(burn_time, thrust, fuel_mass, dry_mass, radius, angle, radius_fins):
@@ -7,12 +7,11 @@ def get_simple_rocket_FINS_change_equation_and_conditions(burn_time, thrust, fue
     # The rate the rocket loses mass due to fuel burn (assumes linear loss)
     fuel_mass_loss = fuel_mass / burn_time
     initial_mass = dry_mass + fuel_mass
-    
-    ANGLE = angle #in degrees - 5
+
+    angle_radians = angle / 180 * pi
 
     r = radius       #radius of the rocket tube in meters - 0.049/2
     r_fins = radius_fins   #radius of the semi-circular fins in meters - 0.04
-    cp = 0.01 + (1.5-0.01)*(ANGLE/90)       #cp constant for the drag force for the fins at an angle (approximated by mel)
     density_air = 1
     r_F = r + 4*r_fins/(3*pi)   #centre of mass of a semicircle + radius of the rocket
 
@@ -22,6 +21,7 @@ def get_simple_rocket_FINS_change_equation_and_conditions(burn_time, thrust, fue
         old_position = x[0]
         old_velocity = x[1]
         mass = x[2]
+        old_angular_acceleration = x[3]
         delta_mass = 0
 
         force = 0
@@ -29,26 +29,30 @@ def get_simple_rocket_FINS_change_equation_and_conditions(burn_time, thrust, fue
             force = thrust
             delta_mass = -fuel_mass_loss
 
+        angle_effective = angle_radians - (0 if old_angular_acceleration == 0 else atan2((old_angular_acceleration * radius), old_velocity))
+
+        cp = 0.01 + (1.5-0.01)*((angle_effective/pi)*(180/90))       #cp constant for the drag force for the fins at an angle (approximated by mel)
 
         #F drag = Area of the fins * Cp * v^2 * rho * 0.5 * sin(angle) * no. of fins
-        F_drag = (pi*r_fins*r_fins*0.5)*cp*old_velocity*old_velocity*density_air*0.5*sin(ANGLE/360*2*pi)*3
+        F_drag = (pi*r_fins*r_fins*0.5)*cp*old_velocity*old_velocity*density_air*0.5*sin(angle_effective)*3
+
+        F_drag_x = sin(angle_effective) * F_drag
+        F_drag_y = cos(angle_effective) * F_drag
 
         #resultant torque acting on the whole rocket
-        torque = r_F * F_drag
+        torque = r_F * F_drag_x
 
         inertia = 0.5 * mass * r*r        #inertia vector, for a uniform cylinder: 0.5 * m * r^2
 
 
         air_resistance = pow(old_velocity, 2) * 1/2 * pow(radius, 2) * pi * cp
 
-        air_resistance += F_drag    #air resistance plus drag from spinning
+        air_resistance += F_drag_y    #air resistance plus drag from spinning
 
         if old_velocity > 0:
             force = force - air_resistance
         else:
             force = force + air_resistance
-
-        
 
         acceleration = force/mass
         acceleration = acceleration - 9.81
--- sensor_data.py
+++ sensor_data.py
@@ -1,68 +1,21 @@
-import datetime
-from pymongo import MongoClient
-import pandas as pd
-import matplotlib.pyplot as plt
- 
-# Provide the mongodb atlas url to connect python to mongodb using pymongo
-CONNECTION_STRING = ""
-ROCKET_NAME = 'Spatula'
-FLIGHT_NAME = 'Flight 2023-05-04T15:09:27.413381'
+import requests
+import json
 
-# Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-client = MongoClient(CONNECTION_STRING)
-
-# Create the database for our example (we will use the same database throughout the tutorial
-db = client['rocketry']
-  
-vessel_collection = db['vessels']
-
-vessel = vessel_collection.find({'name': {'$eq': ROCKET_NAME}})[0]
-
-flight_collection = db['flights']
-
-flight = flight_collection.find({'name': {'$eq': FLIGHT_NAME}, '_vessel_id': { '$eq': str(vessel['_id']) }})[0]
-
-flight_data_collection = db['flight_data_compact']
-
-def get_data_for_part(part_name: str, start: datetime.datetime, end: datetime.datetime):
-
-    part = [ x for x in vessel['parts'] if x['name'] == part_name][0]
-
-    measured = [x for x in flight['measured_parts'].items() if x[0] == part['_id']][0][1]
-
-    data_names = [x['name'] for x in measured]
-
-    raw = list(flight_data_collection.find({'metadata._flight_id': {'$eq': str(flight['_id'])}, 'metadata.part_id': {'$eq': part['_id'] }, '_start_time': { '$gte': start, '$lt': end } }))
-
-    data_agg = {'Time': list[datetime.datetime]()}
-
-    for name in data_names:
-        data_agg[name] = list()
-
-    for d in raw:
-        for m in d['measurements']:
-            data_agg['Time'].append(datetime.datetime.fromtimestamp(m[0]))
-            i = 0
-            for mm in m[1]:
-                data_agg[data_names[i]].append(mm)
-                i += 1
-
-    return pd.DataFrame(data_agg)
-
-flight_1_start = datetime.datetime.fromisoformat('2023-05-04T15:22:00.000+01:00')
-flight_1_end = datetime.datetime.fromisoformat( '2023-05-04T15:22:25.000+01:00')
-
-flight_2_start = datetime.datetime.fromisoformat('2023-05-04T15:22:48.000+01:00')
-flight_2_end = datetime.datetime.fromisoformat( '2023-05-04T15:23:02.000+01:00')
+BASE_URL = 'http://rocketry.sidneypauly.me:5000'
+VESSEL_NAME = 'Spatula (Production)'
+PART_NAME = 'Battery'
+FLIGHT_NAME = 'flight_1'
 
 
-dataframe = get_data_for_part('Gyroscope',flight_2_start, flight_2_end)
-dataframe.plot(x='Time', y=['rotation-x', 'rotation-y', 'rotation-z'])
+vessels_request = requests.get(f'{BASE_URL}/vessel/get_by_name/{VESSEL_NAME}')
+vessels = vessels_request.json()
+vessel = vessels[0]
 
-dataframe = get_data_for_part('Accelerometer', flight_2_start, flight_2_end)
-dataframe.plot(x='Time', y=['acceleration-x', 'acceleration-y', 'acceleration-z'])
+battery_part = [p for p in vessel["parts"] if (PART_NAME in p["name"])][0]
 
-plt.show()
+flights_request = requests.get(f'{BASE_URL}/flight/get_by_name/{vessel["_id"]}/{FLIGHT_NAME}')
+flights = flights_request.json()
+flight = flights[0]
 
-
-print('x')
+measurements_request = requests.get(f'{BASE_URL}/flight_data/get_range/{flight["_id"]}/{battery_part["_id"]}/{flight["start"]}/{flight["end"]}')
+measurements = measurements_request.json()
